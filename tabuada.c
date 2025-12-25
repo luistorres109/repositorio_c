6495b6789311ba73dbb13dde91f7870f09421594
Luís Miguel Torres <94331066+luistorres109@users.noreply.github.com>
2025-12-25 16:06:16 -0300

    Fix formatting and indentation in tabuada.c

--- tabuada.c
+++ tabuada.c
@@ -3,12 +3,12 @@
 
 int main(){
 	int i, j, resultado;
-  for(i = 1; i <= 10; i++){
-    printf("Tabuada do %d:\n", i);
-    for(j = 1; j <= 10; j++){
-      resultado = i * j;
-      printf("- %d X %d = %d\n", i, j, resultado);
-    }
-  }
-  return 0;
+  	for(i = 1; i <= 10; i++){
+    	printf("Tabuada do %d:\n", i);
+    	for(j = 1; j <= 10; j++){
+      		resultado = i * j;
+      		printf("- %d X %d = %d\n", i, j, resultado);
+    	}
+  	}
+  	return 0;
 }
