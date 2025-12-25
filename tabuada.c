d53546a07d5b34ee17fadd1ed1a42a3c36e8a768
Luís Miguel Torres <94331066+luistorres109@users.noreply.github.com>
2025-12-25 16:05:21 -0300

    Refactor multiplication table logic in tabuada.c

--- tabuada.c
+++ tabuada.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 
 int main(){
-  for(int i = 1; i <= 10; i++){
-    printf("Tabuada do %d\n", i);
-    for(int j = 1; j <= 10; j++){
-      printf("- %d X %d = %d\n", i, j, i * j);
+	int i, j, resultado;
+  for(i = 1; i <= 10; i++){
+    printf("Tabuada do %d:\n", i);
+    for(j = 1; j <= 10; j++){
+      resultado = i * j;
+      printf("- %d X %d = %d\n", i, j, resultado);
     }
-    printf("-------------------\n");
   }
   return 0;
 }
